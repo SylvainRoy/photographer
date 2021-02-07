0cc8efe4f76ed0ba58c6df3cad405d5b05d3902f
Sylvain Roy <sylvain.roy@m4x.org>
2021-02-07 21:24:27 +0100

    Client & server basic end to end is OK.

--- server.py
+++ server.py
@@ -5,14 +5,16 @@ A server to provide the service from the web.
 Run it with:
     uvicorn server:app
 or
-    uvicorn main:app --reload
+    uvicorn server:app --reload
 """
 
 from typing import List, Tuple
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
+from optimizer import find_photograper_wsg84
 
 app = FastAPI()
 
@@ -20,10 +22,15 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 class Locate(BaseModel):
     projections: List[float] = []
-    latlng: List[Tuple[float, float]] = []
+    latlngs: List[Tuple[float, float]] = []
 
 @app.post("/locate/")
 async def locate(query: Locate):
-    reply = (12.97, 77.59)
+    """API entry point to locate the photographer."""
+    try:
+        location, error, _ = find_photograper_wsg84(query.latlngs, query.projections)
+        reply = {"location": location, "error": error, "status": "ok"}
+    except RuntimeError as e:
+        reply = {"status": str(e)}
     print("locate request {} => {}".format(query, reply))
-    return {"location": reply}
+    return reply
